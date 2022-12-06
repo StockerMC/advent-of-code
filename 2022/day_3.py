eebecfeca75f9a250a5d7e358ba3e51eefafb6c4
StockerMC <44980366+StockerMC@users.noreply.github.com>
2022-12-06 00:08:28 -0500

    Simplify 2022 day 3

--- 2022/day_3.py
+++ 2022/day_3.py
@@ -1,32 +1,26 @@
-import collections, string, itertools
+import more_itertools
 
 from util import get_input
 lines = get_input(2022, 3)
 
-priorities = {
-    **{letter: ord(letter) - 96 for letter in string.ascii_lowercase},
-    **{letter: ord(letter) - 38 for letter in string.ascii_uppercase}
-}
+def priority(letter: str):
+    return ord(letter) - 96 if letter.islower() else ord(letter) - 38
 
 def part_one():
     score = 0
     for line in lines:
         half = len(line) // 2
         a, b = line[:half], line[half:]
-        same = set(a).intersection(set(b)).pop()
-        score += priorities[same]
+        same = set(a) & set(b)
+        score += priority(same.pop())
 
     print(score)
 
 def part_two():
     score = 0
-    sets = []
-    for i, line in enumerate(lines, start=1):
-        line = line.rstrip()
-        sets.append(set(line))
-        if i % 3 == 0:
-            same = sets[0].intersection(sets[1]).intersection(sets[2]).pop()
-            score += priorities[same]
-            sets = []
+    for group in more_itertools.chunked(lines, 3):
+        sets = [set(line) for line in group]
+        same = sets[0] & sets[1] & sets[2]
+        score += priority(same.pop())
 
     print(score)
