5de76a74fd9a2c4299f97851837f416efd79f334
StockerMC <44980366+StockerMC@users.noreply.github.com>
2023-12-03 14:48:00 -0500

    Clean up day 2 slightly

--- 2023/day_2.py
+++ 2023/day_2.py
@@ -2,29 +2,24 @@ import collections, string, itertools, math, more_itertools, re, functools
 
 def part_one(lines: list[str]):
     sum = 0
-    i = 0
-    for line in lines:
-        i += 1
+    for i, line in enumerate(lines, start=1):
         good = True
         for sub in line.split(';'):
             a = {'red': 0, 'blue': 0, 'green': 0}
             for b in a.keys():
                 for m in re.findall(rf'(\d+) ({b})', sub):
                     a[m[1]] += int(m[0])
-            if not (a['red'] <= 12 and a['green'] <= 13 and a['blue'] <= 14):
+            if a['red'] > 12 or a['green'] > 13 or a['blue'] > 14:
                 good = False
                 break
         if good:
             sum += i
-            print(i)
     return sum
 
 def part_two(lines: list[str]):
     sum = 0
-    i = 0
-    for line in lines:
+    for i, line in enumerate(lines, start=1):
         mina = minb = minc = 1
-        i += 1
         for sub in line.split(';'):
             a = {'red': 0, 'blue': 0, 'green': 0}
             for b in a.keys():
